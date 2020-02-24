a35733cb7f00b44545e1858f7cd837b0b2fbca4b
Bikash Sharma <36207673+bikashxsharma@users.noreply.github.com>
2020-02-24 13:47:52 +0200

    redux installed

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./main.css"
+
+function Main(){
+    return(
+       <main className="main">
+           Main stuff
+       </main>
+    )
+}
+
+
+export default Main;
---
+++ src/sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./sidebar.css"
+
+function Sidebar(){
+    return <aside className="sidebar">Sidebar</aside>
+}
+
+
+
+export default Sidebar;
